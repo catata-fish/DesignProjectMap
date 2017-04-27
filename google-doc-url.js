5a55ab4770e8f32d84467847bd74168027275bda
catata-fish <amir.goldstein@web.de>
2017-04-27 09:56:07 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1K0f-H6ssHg1tb9PccF7HjCb6fEmoXZv0ayrA4VE3d2k/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1OPpzxMos0ukReiFRQyrBn-2S3E6xprxlKj3LLSLpLvw/edit#gid=0';
